336cc66b3cae2448d8a984a076d2ee921a946760
plusun <tomsun.0.7@gmail.com>
2018-07-21 09:04:40 +0000

    fuzz nvi regex(3)

---
+++ tests/fuzz/regex/nvi/regcomp/fuzz_regcomp.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "regex.h"
+
+void init(const char *d, int flag) {
+    regex_t reg;
+    if (regcomp(&reg, d, flag) == 0) {
+        regfree(&reg);
+    }
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    char *d = (char *)malloc(size + 1);
+    memcpy(d, data, size);
+    d[size] = '\0';
+    init(d, REG_BASIC | REG_NOSUB);
+    free(d);
+    return 0;
+}
---
+++ tests/fuzz/regex/nvi/regexec/fuzz_regexec.c
@@ -0,0 +1,52 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "regex.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    size_t i;
+    for (i = 0; i < size; i++) {
+        if (data[i] == '\n') {
+            break;
+        }
+    }
+
+    if (i + 1 >= size) {
+        return 0;
+    }
+
+    char *d = (char *)malloc(size + 1);
+    if (d == NULL) {
+        return 0;
+    }
+    memcpy(d, data, size);
+
+    d[i] = '\0';
+#ifdef _REPRODUCE_
+    printf("pattern: %s\n", d);
+#endif
+    regex_t reg;
+    if (regcomp(&reg, d, REG_BASIC | REG_NOSUB) != 0) {
+        free(d);
+        return 0;
+    }
+
+    size_t len = size - i - 1;
+    char *text = d + i + 1;
+    text[len] = '\0';
+#ifdef _REPRODUCE_
+    printf("text: %s\n", text);
+#endif
+    if (regexec(&reg, text, 0, NULL, 0) != 0) {
+        // do nothing
+        // just continue the execution of free and regfree
+    }
+
+    free(d);
+    regfree(&reg);
+
+    return 0;
+}
