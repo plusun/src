3899801db97939780073ec9089b15d7f7c7ad389
plusun <tomsun.0.7@gmail.com>
2018-07-27 11:36:19 +0000

    Fuzzing agrep regex(3)

---
+++ tests/fuzz/regex/agrep/regcomp/fuzz_regcomp.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "regex.h"
+
+void init(const char *d, int flag) {
+    regex_t reg;
+    if (regcomp(&reg, d, flag) == 0) {
+        regfree(&reg);
+    }
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    char *d = (char *)malloc(size + 1);
+    memcpy(d, data, size);
+    d[size] = '\0';
+    init(d, REG_NOSUB);
+    free(d);
+    return 0;
+}
+
+const char *__MKFUZZER_gettext(const char *str) {
+    return str;
+}
